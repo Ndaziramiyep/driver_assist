89321b42a787920de41b8e9e9d189108547e8982
Ndaziramiyep <patrickofficial30@gmail.com>
2025-04-15 12:22:28 +0200

    Adding home_screen codesfile

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,1 +1,56 @@
 // Map + action buttons
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController mapController;
+  LatLng? currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (!serviceEnabled || permission == LocationPermission.deniedForever) {
+      return;
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.always &&
+          permission != LocationPermission.whileInUse) {
+        return;
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      currentPosition = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void _onActionTap(String action) {
+    // TODO: Navigate to respective feature pages
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Clicked on $action')),
+    );
+  }
